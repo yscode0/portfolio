dfc2da5e6ef679ab4b9baae88ddea9f10c2c9a07
yscode0 <runa.miyunopapa555@gmail.com>
2026-03-16 22:15:54 +0900

    トップページ

--- asset/js/main.js
+++ asset/js/main.js
@@ -33,7 +33,7 @@ function initCustomCursor() {
     rafId = requestAnimationFrame(animRing);
   })();
 
-  document.querySelectorAll('a, button').forEach(el => {
+  document.querySelectorAll('a, button, .p-top__about-skill-item').forEach(el => {
     el.addEventListener('mouseenter', () => document.body.classList.add('is-hove'));
     el.addEventListener('mouseleave', () => document.body.classList.remove('is-hove'));
   });
