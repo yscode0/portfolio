b275c341b084f33a29dec08383c7d2e043a28674
yscode0 <runa.miyunopapa555@gmail.com>
2026-04-01 10:27:51 +0900

    アバウトページ作成、p-top__about-skill-tagsをc-skill-tagsにコンポーネント化

--- asset/js/main.js
+++ asset/js/main.js
@@ -32,7 +32,7 @@ function initCustomCursor() {
     rafId = requestAnimationFrame(animRing);
   })();
 
-  document.querySelectorAll('a, button, .p-top__about-skill-item').forEach(el => {
+  document.querySelectorAll('a, button, .js-hove').forEach(el => {
     el.addEventListener('mouseenter', () => document.body.classList.add('is-hove'));
     el.addEventListener('mouseleave', () => document.body.classList.remove('is-hove'));
   });
